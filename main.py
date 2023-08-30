a50ba11cb648b9a65aaeddf93d4dbe0482ec58a8
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-08-30 23:55:31 +0530

    Removed the initial bugs.

--- main.py
+++ main.py
@@ -1,59 +1,10 @@
-"""
-while True:
-    print("1. Create")
-    print("2. Update")
-    ch = int(input("Enter your choice: "))
-
-    if ch == 1:
-        print("Create")
-    elif ch == 2:
-        print("Update")
-    elif ch == 3:
-        print("Exit")
-        break
-
-"""
-
-"""
-a = 1.21
-b = 220.44
-print("Balance Expense")
-print("%7.2f %7.2f" % (a, b))
-"""
-"""
-import os
 import datetime
-import time
 
-file_path = "pass.txt"
 
-if not os.path.exists(file_path):
-    file = open("pass.txt", "w")
-    file.write(" Balance  Expense     Gain \t\tDate and Time \n")
+def print_pass():
+    file = open("pass.txt", "r")
+    print(file.read())
     file.close()
-
-file = open("pass.txt", "a")
-file.write("%8.2f %8.2f %8.2f \t%s\n" % (100.09, 20.55, 43.4, datetime.datetime.now()))
-file = open("pass.txt", "r")
-print(file.read())
-file.close()
-"""
-"""
-file = open("pass.txt", "r")
-obj = ""
-for i in file:
-    obj = i
-
-print(obj)
-
-obj = obj.lstrip()
-
-num = obj.split(" ")
-
-print(num[0])
-"""
-import datetime
-import time
 
 
 class Passbook:
@@ -67,7 +18,7 @@ class Passbook:
             file = open("pass.txt", "w")
             file.write(" Balance  Expense     Gain \t\tDate and Time \n")
             file.close()
-            balance = float(input("Enter the initial balance: "))
+            self.balance = float(input("Enter the initial balance: "))
         else:
             file = open("pass.txt", "r")
             obj = ""
@@ -75,28 +26,33 @@ class Passbook:
                 obj = i
             obj = obj.lstrip()
             num = obj.split(" ")
-            balance = num[0]
+            self.balance = float(num[0])
             file.close()
 
     def withdraw(self):
         with_amount = float(input("Enter the amount: "))
         if with_amount > self.balance:
             print("Enter a valid amount")
-            print("Your current balance is",self.balance)
+            print("Your current balance is", self.balance)
         else:
             self.balance -= with_amount
-            file = open("pass.txt", "w")
+            file = open("pass.txt", "a")
             file.write("%8.2f %8.2f %8.2f \t%s\n" % (self.balance, with_amount, 0, datetime.datetime.now()))
+            file.close()
 
-    def deposite(self):
+    def deposit(self):
+        depo_amount = float(input("Enter the amount: "))
+        self.balance += depo_amount
+        file = open("pass.txt", "a")
+        file.write("%8.2f %8.2f %8.2f \t%s\n" % (self.balance, 0, depo_amount, datetime.datetime.now()))
+        file.close()
 
 
-
-
+print("Welcome to your own bookkeeper")
 book = Passbook()
 
 while True:
-    print("----------MENU----------")
+    print("\n----------MENU----------")
     print("1. Print Balance Book")
     print("2. Register a Expense")
     print("3. Register a Gain")
@@ -104,9 +60,11 @@ while True:
     ch = int(input("Enter your choice: "))
 
     if ch == 1:
-        print("Create")
+        print_pass()
     elif ch == 2:
-        print("Update")
+        book.withdraw()
     elif ch == 3:
-        print("Exit")
+        book.deposit()
+    elif ch == 4:
+        print("Exit....")
         break
